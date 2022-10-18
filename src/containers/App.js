ae532de1b27e50522195f4d529e342a1c0688409
Lois Graham <loisgraham12@gmail.com>
2022-10-18 11:22:45 +0100

    File cleanup and destructuring

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import CardList from './CardList';
-import SearchBox from './SearchBox';
-import Scroll from './Scroll';
-import { robots } from './robots';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import Scroll from '../components/Scroll';
+import { robots } from '../robots';
 import './App.css';
 
 // class App extends React.component {
@@ -38,10 +38,11 @@ componentDIDMount() {
   }
 
   render() {
-    const filteredRobots = this.state.robots.filter(robot =>{
-      return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+    const { robots, searchfield } = this.state;
+    const filteredRobots = robots.filter(robot =>{
+      return robot.name.toLowerCase().includes(searchfield.toLowerCase());
     })
-    if (this.state.robots.length === 0) {
+    if (robots.length === 0) {
       return <h1>Loading</h1>
     } else {
       return (
